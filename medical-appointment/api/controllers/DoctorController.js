fa559d5a6923d7ccd6fbc4dd01c6a9274f00e0c1
Splav555 <olegka35@gmail.com>
2018-10-23 08:46:20 +0300

    get doctor timesheet

--- medical-appointment/api/controllers/DoctorController.js
+++ medical-appointment/api/controllers/DoctorController.js
@@ -6,9 +6,10 @@
  */
 
 let QUERY = `
-      SELECT ${Doctor.tableName}.*, 
+      SELECT ${Doctor.tableName}.*, ${Speciality.tableName}.${Speciality.schema.name.columnName} AS speciality,
       ${User.tableName}.${User.schema.last_name.columnName}, ${User.tableName}.${User.schema.first_name.columnName}, ${User.tableName}.${User.schema.middle_name.columnName}
       FROM ${Doctor.tableName}
+      JOIN ${Speciality.tableName} ON ${Speciality.tableName}.${Speciality.schema.id.columnName} = ${Doctor.tableName}.${Doctor.schema.speciality.columnName}
       JOIN ${User.tableName} ON ${User.tableName}.${User.schema.id.columnName} = ${Doctor.tableName}.${Doctor.schema.id.columnName}`;
 
 module.exports = {
@@ -31,7 +32,21 @@ module.exports = {
   },
 
   doctor_info: async function(req, res) {
-    
+    let TIMESHEET_QUERY = "SELECT mc_id, date, start FROM timesheet WHERE doctor_id = $1 AND mc_id = $2 AND date >= current_date";
+    let id = parseInt(req.param('doctor_id'));
+    let mc = parseInt(req.param('mc', 0));
+    if(isNaN(id)) {
+      return res.badRequest('Incorrect doctor ID');
+    }
+    else {
+      let whereClause = `WHERE ${Doctor.tableName}.${Doctor.schema.id.columnName} = $1 LIMIT 1`;
+      let doctors = await sails.sendNativeQuery(QUERY + " " + whereClause, [id]);
+      if(doctors.rowCount === 0) return res.notFound("Doctor with ID " + id + " is not found");
+
+      let timesheet = await sails.sendNativeQuery(TIMESHEET_QUERY, [id, mc]);
+      doctors.rows[0].timesheet = timesheet.rows;
+      return res.json(doctors.rows[0]);
+    }
   }
 };
 
--- medical-appointment/config/routes.js
+++ medical-appointment/config/routes.js
@@ -17,5 +17,6 @@ module.exports.routes = {
   'GET /users/all': 'UserController.all_users',
   //'GET /users/:name': 'UserController.find_users_by_name',
   'GET /doctors/all': 'DoctorController.all_doctors',
+  'GET /doctors/:doctor_id': 'DoctorController.doctor_info',
   'GET /doctors/': 'DoctorController.find_doctors'
 };
