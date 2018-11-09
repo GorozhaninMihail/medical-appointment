46e548285e29014f125068b32b292972af392a6e
Oleg Tarasov <olegka35@gmail.com>
2018-11-09 15:09:04 +0300

    Update DoctorController.js

--- api/controllers/DoctorController.js
+++ api/controllers/DoctorController.js
@@ -5,7 +5,7 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-let QUERY = `
+const QUERY = `
       SELECT ${Doctor.tableName}.*, ${Speciality.tableName}.${Speciality.schema.name.columnName} AS speciality,
       ${User.tableName}.${User.schema.last_name.columnName}, ${User.tableName}.${User.schema.first_name.columnName}, ${User.tableName}.${User.schema.middle_name.columnName}
       FROM ${Doctor.tableName}
@@ -13,40 +13,41 @@ let QUERY = `
       JOIN ${User.tableName} ON ${User.tableName}.${User.schema.id.columnName} = ${Doctor.tableName}.${Doctor.schema.id.columnName}`;
 
 module.exports = {
-  all_doctors: async function(req, res)
-  {
-    let doctors = await sails.sendNativeQuery(QUERY);
+  async allDoctors(req, res) {
+    const doctors = await sails.sendNativeQuery(QUERY);
     return res.json(doctors.rows);
   },
 
-  find_doctors: async function(req, res) {
-    let mc = parseInt(req.param('mc'));
-    if(isNaN(mc)) {
+  async findDoctors(req, res) {
+    const mc = parseInt(req.param('mc'));
+    
+    if (isNaN(mc)) {
       return res.badRequest('Incorrect medical centre ID');
     }
-    else {
-      let whereClause = `WHERE ${Doctor.tableName}.${Doctor.schema.id.columnName} IN (SELECT doctor_id FROM doctors_centres WHERE centre_id = $1)`;
-      let doctors = await sails.sendNativeQuery(QUERY + " " + whereClause, [mc]);
-      return res.json(doctors.rows);
-    }
+    
+    const whereClause = `WHERE ${Doctor.tableName}.${Doctor.schema.id.columnName} IN (SELECT doctor_id FROM doctors_centres WHERE centre_id = $1)`;
+    const doctors = await sails.sendNativeQuery(QUERY + " " + whereClause, [mc]);
+    return res.json(doctors.rows);
   },
-
-  doctor_info: async function(req, res) {
-    let TIMESHEET_QUERY = "SELECT mc_id, date, start FROM timesheet WHERE doctor_id = $1 AND mc_id = $2 AND date >= current_date";
-    let id = parseInt(req.param('doctor_id'));
-    let mc = parseInt(req.param('mc', 0));
-    if(isNaN(id)) {
+  
+  async doctorInfo(req, res) {
+    const TIMESHEET_QUERY = "SELECT mc_id, date, start FROM timesheet WHERE doctor_id = $1 AND mc_id = $2 AND date >= current_date";
+    const id = parseInt(req.param('doctor_id'));
+    const mc = parseInt(req.param('mc', 0));
+    
+    if (isNaN(id)) {
       return res.badRequest('Incorrect doctor ID');
     }
-    else {
-      let whereClause = `WHERE ${Doctor.tableName}.${Doctor.schema.id.columnName} = $1 LIMIT 1`;
-      let doctors = await sails.sendNativeQuery(QUERY + " " + whereClause, [id]);
-      if(doctors.rowCount === 0) return res.notFound("Doctor with ID " + id + " is not found");
-
-      let timesheet = await sails.sendNativeQuery(TIMESHEET_QUERY, [id, mc]);
-      doctors.rows[0].timesheet = timesheet.rows;
-      return res.json(doctors.rows[0]);
+    
+    const whereClause = `WHERE ${Doctor.tableName}.${Doctor.schema.id.columnName} = $1 LIMIT 1`;
+    const doctors = await sails.sendNativeQuery(QUERY + " " + whereClause, [id]);
+    if (doctors.rowCount === 0) {
+      return res.notFound(`Doctor with ID ${id} was not found`);
     }
+
+    const timesheet = await sails.sendNativeQuery(TIMESHEET_QUERY, [id, mc]);
+    doctors.rows[0].timesheet = timesheet.rows;
+    return res.json(doctors.rows[0]);
   }
 };
 
