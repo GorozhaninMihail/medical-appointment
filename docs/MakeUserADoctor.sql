df723cd42041b89605ce1c3f3f11a34b0aa3ec14
GorozhaninMihail <gorozhaninmihail@yandex.ru>
2019-01-13 21:35:49 +0300

    Добавлены файлы спец процедур
    Удаление доктора и добавление доктора

---
+++ docs/MakeUserADoctor.sql
@@ -0,0 +1,23 @@
+﻿CREATE OR REPLACE FUNCTION public.MakeUserADoctor(ID int, speciality int, experience int, information TEXT DEFAULT NULL) RETURNS void
+LANGUAGE plpgsql
+AS $plpgsql$
+DECLARE 
+  vxActive bool;
+  vxType Text;
+  vxRc int;
+BEGIN
+  Select type into vxType from public.users where user_id = ID;
+  if vxType = 'user' then
+  UPDATE public.users SET type = 'doctor' WHERE user_id = ID;  
+  Select active into vxActive from public.doctors where doctor_id=ID and speciality_id = speciality;
+  GET DIAGNOSTICS vxRc = ROW_COUNT;
+  if vxRc = 0 Then 
+  INSERT INTO public.doctors VALUES
+    (ID, speciality, experience, information, true);
+  end if;
+if  vxActive !=true then
+  UPDATE public.doctors SET active = true WHERE doctor_id = ID and speciality_id = speciality;
+end if; 
+end if;
+END;
+$plpgsql$;
---
+++ docs/RemoveDoctor.sql
@@ -0,0 +1,22 @@
+﻿CREATE OR REPLACE FUNCTION public.RemoveDoctor(ID int) RETURNS void
+LANGUAGE plpgsql
+AS $plpgsql$
+DECLARE 
+  vxType Text;
+  vxRc int;
+BEGIN
+  Select type into vxType from public.users where user_id = ID;
+  if vxType = 'doctor' then
+  UPDATE public.users SET type = 'user' WHERE user_id = ID;
+  GET DIAGNOSTICS vxRc = ROW_COUNT;
+  if vxRc = 0 Then 
+  return;
+  end if;
+  UPDATE public.doctors SET active = false WHERE doctor_id = ID;
+  GET DIAGNOSTICS vxRc = ROW_COUNT;
+  if vxRc = 0 Then 
+  return;
+  end if; 
+end if;
+END;
+$plpgsql$;
